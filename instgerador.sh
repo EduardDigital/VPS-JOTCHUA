4e261499fa59eba9ea4af8ecd251411ce89be84b
EduardDigital <104742695+EduardDigital@users.noreply.github.com>
2022-09-18 11:58:10 -0500

    Update instgerador.sh

--- instgerador.sh
+++ instgerador.sh
@@ -12,60 +12,60 @@ echo -e "$BARRA"
 echo -e "\033[92m        -- INSTALANDO PAQUETES NECESARIOS -- "
 echo -e "$BARRA"
 #bc
-[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "bc"|head -1)  || apt-get install bc -y &>/dev/null
+ $(dpkg --get-selections|grep -w "bc"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "bc"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install bc................... $ESTATUS "
 #screen
-[[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] || apt-get install screen -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "screen"|head -1)  || apt-get install screen -y &>/dev/null
+ $(dpkg --get-selections|grep -w "screen"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "screen"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install screen............... $ESTATUS "
 #nano
-[[ $(dpkg --get-selections|grep -w "nano"|head -1) ]] || apt-get install nano -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "nano"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "nano"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "nano"|head -1)  || apt-get install nano -y &>/dev/null
+ $(dpkg --get-selections|grep -w "nano"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "nano"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install nano................. $ESTATUS "
 #curl
-[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || apt-get install curl -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "curl"|head -1)  || apt-get install curl -y &>/dev/null
+ $(dpkg --get-selections|grep -w "curl"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "curl"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install curl................. $ESTATUS "
 #netcat
-[[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] || apt-get install netcat -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "netcat"|head -1)  || apt-get install netcat -y &>/dev/null
+ $(dpkg --get-selections|grep -w "netcat"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "netcat"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install netcat............... $ESTATUS "
 #netcat-traditional
-[[ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1) ]] || apt-get install netcat-traditional -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1)  || apt-get install netcat-traditional -y &>/dev/null
+ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install netcat-traditional... $ESTATUS "
 #net-tools
-[[ $(dpkg --get-selections|grep -w "net-tools"|head -1) ]] || apt-get install net-tools -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "net-tools"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "net-tools"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "net-tools"|head -1)  || apt-get install net-tools -y &>/dev/null
+ $(dpkg --get-selections|grep -w "net-tools"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "net-tools"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install net-tools............ $ESTATUS "
 #cowsay
-[[ $(dpkg --get-selections|grep -w "cowsay"|head -1) ]] || apt-get install cowsay -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "cowsay"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "cowsay"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "cowsay"|head -1)  || apt-get install cowsay -y &>/dev/null
+ $(dpkg --get-selections|grep -w "cowsay"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "cowsay"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install cowsay............... $ESTATUS "
 #figlet
-[[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] || apt-get install figlet -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "figlet"|head -1)  || apt-get install figlet -y &>/dev/null
+ $(dpkg --get-selections|grep -w "figlet"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "figlet"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install figlet............... $ESTATUS "
 #lolcat
 apt-get install lolcat -y &>/dev/null
 sudo gem install lolcat &>/dev/null
-[[ $(dpkg --get-selections|grep -w "lolcat"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "lolcat"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "lolcat"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "lolcat"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install lolcat............... $ESTATUS "
 #apache2
-[[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] || apt-get install apache2 -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
+ $(dpkg --get-selections|grep -w "apache2"|head -1)  || apt-get install apache2 -y &>/dev/null
+ $(dpkg --get-selections|grep -w "apache2"|head -1)  || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
+ $(dpkg --get-selections|grep -w "apache2"|head -1)  && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 echo -e "\033[97m  # apt-get install apache2.............. $ESTATUS "
 sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
 service apache2 restart > /dev/null 2>&1 &
@@ -75,7 +75,7 @@ echo -e "$BARRA"
 echo -e "\033[97m Si la instalacion de paquetes tiene fallas"
 echo -ne "\033[97m Puede intentar de nuevo [s/n]: "
 read inst
-[[ $inst = @(s|S|y|Y) ]] && install_ini
+ $inst = @(s|S|y|Y)  && install_ini
 }
 
 ofus () {
@@ -103,8 +103,8 @@ echo "$txtofus" | rev
 }
 
 veryfy_fun () {
-[[ ! -d ${IVAR} ]] && touch ${IVAR}
-[[ ! -d ${SCPT_DIR} ]] && mkdir ${SCPT_DIR}
+ ! -d ${IVAR}  && touch ${IVAR}
+ ! -d ${SCPT_DIR}  && mkdir ${SCPT_DIR}
 unset ARQ
 case $1 in
 "gerar.sh")ARQ="/usr/bin/";;
@@ -118,22 +118,22 @@ chmod +x ${ARQ}/$1
 meu_ip () {
 MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 MIP2=$(wget -qO- ipv4.icanhazip.com)
-[[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
+ "$MIP" != "$MIP2"  && IP="$MIP2" || IP="$MIP"
 echo "$IP" > /usr/bin/vendor_code
 }
 
 function_verify () {
   permited=$(curl -sSL "https://raw.githubusercontent.com/EduardDigital/VPS-JOTCHUA/master-1/control-IP")
-  [[ $(echo $permited|grep "${IP}") = "" ]] && {
+   $(echo $permited|grep "${IP}") = ""  && {
   echo -e "\n\n\n\033[1;31m====================================================="
   echo -e "\033[1;31m       ¡LA IP $(wget -qO- ipv4.icanhazip.com) NO ESTA AUTORIZADA!"
-  echo -e "\033[1;31m                CONTACTE A @Rufu99"
+  echo -e "\033[1;31m                CONTACTE A @Jotchua_MRX"
   echo -e "\033[1;31m=====================================================\n\n\n"
-  [[ -d /etc/SCRIPT ]] && rm -rf /etc/SCRIPT
+   -d /etc/SCRIPT  && rm -rf /etc/SCRIPT
   exit 1
   } || {
   ### INTALAR VERCION DE SCRIPT
-  v1=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/Generador_Gen_VPS-MX/master/Vercion")
+  v1=$(curl -sSL "https://raw.githubusercontent.com/EduardDigital/VPS-JOTCHUA/master-1/Vercion")
   echo "$v1" > /etc/versin_script
   }
 }
@@ -149,23 +149,23 @@ cd $HOME
 REQUEST=$(echo $SCPresq|$SUB_DOM)
 wget -O "$HOME/lista-arq" ${REQUEST}/GERADOR > /dev/null 2>&1
 sleep 1s
-[[ -e $HOME/lista-arq ]] && {
+ -e $HOME/lista-arq  && {
 for arqx in `cat $HOME/lista-arq`; do
 echo -ne "\033[1;33mDescargando: \033[1;31m[$arqx] "
 wget -O $HOME/$arqx ${REQUEST}/${arqx} > /dev/null 2>&1 && {
 echo -e "\033[1;31m- \033[1;32mRecibido!"
-[[ -e $HOME/$arqx ]] && veryfy_fun $arqx
+ -e $HOME/$arqx  && veryfy_fun $arqx
 } || echo -e "\033[1;31m- \033[1;31mFalla (no recibido!)"
 done
-[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://raw.githubusercontent.com/rudi9999/Generador_Gen_VPS-MX/master/Install/trans &> /dev/null
-[[ -e /bin/http-server.py ]] && mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
+ ! -e /usr/bin/trans  && wget -O /usr/bin/trans https://raw.githubusercontent.com/EduardDigital/VPS-JOTCHUA/master-1/trans &> /dev/null
+ -e /bin/http-server.py  && mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
 IVAR2="/etc/key-gerador"
 echo "$Key" > $IVAR
 # cp $HOME/lista-arq /etc/SCRIPT
 cp /bin/http-server.sh /etc/SCRIPT
 mv /etc/SCRIPT/http-server.sh /etc/SCRIPT/http-server.py
 chmod +x /etc/SCRIPT/http-server.py
-wget https://raw.githubusercontent.com/rudi9999/Generador_Gen_VPS-MX/master/gerador/gerar.sh &>/dev/null
+wget https://raw.githubusercontent.com/EduardDigital/VPS-JOTCHUA/master-1/gerar.sh &>/dev/null
 mv gerar.sh /etc/SCRIPT
 chmod +x /etc/SCRIPT/gerar.sh
 cd /etc/SCRIPT/
